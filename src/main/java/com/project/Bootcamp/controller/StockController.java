de88559c442cf80f78c56cf1ee815a9b324d4135
Guilherme de Jesus <guilherme.ichiyon@gmail.com>
2021-05-28 17:13:35 -0300

    Código Java do Bootcamp

---
+++ src/main/java/com/project/Bootcamp/controller/StockController.java
@@ -0,0 +1,51 @@
+package com.project.Bootcamp.controller;
+
+
+import java.util.List;
+import com.project.Bootcamp.service.StockService;
+import com.project.Bootcamp.model.dto.StockDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
+
+@CrossOrigin
+@RestController
+@RequestMapping(value= "/stock")
+public class StockController {
+
+    @Autowired
+    private StockService service;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<StockDTO> save(@Valid @RequestBody StockDTO dto){
+        return ResponseEntity.ok(service.save(dto));
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<StockDTO> update(@Valid @RequestBody StockDTO dto){
+        return ResponseEntity.ok(service.update(dto));
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<StockDTO>> findAll(){
+
+        return ResponseEntity.ok(service.findALl());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<StockDTO> findById(@PathVariable Long id){
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @DeleteMapping(value = "/{id}", produces =MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<StockDTO> delete(@PathVariable Long id){
+        return ResponseEntity.ok(service.delete(id));
+    }
+
+    @GetMapping(value = "/today")
+    public ResponseEntity<List<StockDTO>> findByToday(){
+        return ResponseEntity.ok(service.findBYToday());
+    }
+}
